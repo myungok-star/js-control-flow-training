d6edf4d67b55f435217f3631a56e5e5f2df8fbb1
myungok-star <myungok.star@gmail.com>
2016-10-26 23:25:07 -0700

    completed login.js and security_quesitons parts

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,16 @@
 console.log("login.js loaded");
+
+var userLogin = {userName: "btwnthbrs", password: "helloworld"};
+var passwordInput;
+var loggedIn = false;
+
+while (!loggedIn) {
+  passwordInput = prompt("Enter password for user " + userLogin.userName + ".")
+  if (passwordInput === userLogin.password){
+    console.log("Passwords match!");
+
+  loggedIn = true;
+} else {
+    console.log("Passwords do not match. Try again!");
+  }
+}
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,8 +1,26 @@
 console.log("security_questions.js loaded");
 
 var securityQuestions = [
-  {question: "In what city or town did your mother and father meet?", expectedAnswer: "San Francisco"};
-  {question: "What was your high school mascot?", expectedAnswer: "Huskey"};
-  {question: "Who is your favorite movie character?", expectedAnswer: "Furiosa"};
-  {question: "What year did you buy your first car?", expectedAnswer: "2016"};
+  {question: "In what city or town did your mother and father meet?", expectedAnswer: "San Francisco"},
+  {question: "What was your high school mascot?", expectedAnswer: "Huskey"},
+  {question: "Who is your favorite movie character?", expectedAnswer: "Furiosa"},
+  {question: "What year did you buy your first car?", expectedAnswer: "2016"}
 ]
+
+var userAnswer = "";
+var wrongUser= false; //How do I come up with this variable?
+
+for (var i=0; i < securityQuestions.length; i++){
+    userAnswer = prompt(securityQuestions[i].question);
+      if (userAnswer !== securityQuestions[i].expectedAnswer){
+          alert("Incorrect security quesiton response!");
+
+      wrongUser = true; //Why do we need this assignment?
+        break;
+      }
+
+}
+
+if (wrongUser) {
+  console.log("Stop!");
+}
