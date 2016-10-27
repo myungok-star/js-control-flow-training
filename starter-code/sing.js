72b71f1528f5523e4e84f967d3d59aa07e4080d6
myungok-star <myungok.star@gmail.com>
2016-10-26 23:52:32 -0700

    finished sing.js project

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -8,4 +8,13 @@ while (numBottles > 0) {
     console.log(numBottles + " " + bottles + " of beer on the wall,");
     console.log(numBottles + " " + bottles + " of beer!");
     console.log("Take one down and pass it around,");
-}
+    numBottles = numBottles - 1;
+    if (numBottles === 1) {
+        bottles = "bottle"
+    }
+    if (numBottles === 0) {
+        console.log("No more bottles of beer on the wall!");
+      } else {
+        console.log(numBottles + " " + "bottles" + " of beer on the wall!")
+      }
+    }
