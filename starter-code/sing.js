fef60b1df22b83d3b1b8537d44664ff2f4b70efe
myungok-star <myungok.star@gmail.com>
2016-10-26 23:43:44 -0700

    beginning of sing.js homework

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,11 @@
 console.log("sing.js loaded");
+
+
+var numBottles = prompt("How many bottles of beer on the wall?");
+
+var bottles = "bottles";
+while (numBottles > 0) {
+    console.log(numBottles + " " + bottles + " of beer on the wall,");
+    console.log(numBottles + " " + bottles + " of beer!");
+    console.log("Take one down and pass it around,");
+}
