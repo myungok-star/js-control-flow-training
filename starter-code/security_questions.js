ee11bdb33815a0b21d19bea9e5e5842c7df10f6c
myungok-star <myungok.star@gmail.com>
2016-10-26 21:03:44 -0700

    Created an array with 4 objects.

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,8 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+  {question: "In what city or town did your mother and father meet?", expectedAnswer: "San Francisco"};
+  {question: "What was your high school mascot?", expectedAnswer: "Huskey"};
+  {question: "Who is your favorite movie character?", expectedAnswer: "Furiosa"};
+  {question: "What year did you buy your first car?", expectedAnswer: "2016"};
+]
